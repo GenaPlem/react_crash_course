822d49dc81f51e98cdf858f1e349ce8a5a4b9197
GenaPlem <shainkill1337@gmail.com>
2024-10-15 17:12:54 +0100

    Add state for text and handler of textarea

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 const NewPost = () => {
-  return (
-    <form className={classes.form}>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
-      </p>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
-      </p>
-    </form>
-  );
+    const [text, setText] = useState('');
+
+    const changeBodyHandler = (event) => {
+        setText(event.target.value);
+    }
+    
+    return (
+        <form className={classes.form}>
+        <p>
+            <label htmlFor="body">Text</label>
+            <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+        </p>
+        <p>{text}</p>
+        <p>
+            <label htmlFor="name">Your name</label>
+            <input type="text" id="name" required />
+        </p>
+        </form>
+    );
 }
 
 export default NewPost;
