4bc3d9188444169463159754e67fe5c1a47dc97a
GenaPlem <shainkill1337@gmail.com>
2024-10-15 16:57:05 +0100

    Add NewPost component and styles

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from './PostsList.module.css'
 
 const PostsList = () => {
     return (
-        <ul className={styles.posts}>
-            <Post author='Maxim' body='React is awesome!'/>
-            <Post author='David' body='Check it, it is awesome!'/>
-        </ul>
+        <>
+            <NewPost />
+            <ul className={styles.posts}>
+                <Post author='Maxim' body='React is awesome!'/>
+                <Post author='David' body='Check it, it is awesome!'/>
+            </ul>
+        </>
     )
 }
 
