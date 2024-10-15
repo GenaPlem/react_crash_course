60f28df270ba3b43b65de337e7be58dd825390a6
GenaPlem <shainkill1337@gmail.com>
2024-10-15 17:41:58 +0100

    Add Modal component and styles

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,29 +1,35 @@
-import { useState } from 'react';
+import { useState } from "react";
 import NewPost from "./NewPost";
+import Modal from "./Modal";
 import Post from "./Post";
-import styles from './PostsList.module.css'
+import styles from "./PostsList.module.css";
 
 const PostsList = () => {
-    const [text, setText] = useState('');
-    const [author, setAuthor] = useState('');
+  const [text, setText] = useState("");
+  const [author, setAuthor] = useState("");
 
-    const changeBodyHandler = (event) => {
-        setText(event.target.value);
-    }
-    
-    const changeAuthorHandler = (e) => {
-        setAuthor(e.target.value);
-    }
+  const changeBodyHandler = (event) => {
+    setText(event.target.value);
+  };
 
-    return (
-        <>
-            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
-            <ul className={styles.posts}>
-                <Post author={author} body={text}/>
-                <Post author='David' body={'Check it, it is awesome!'}/>
-            </ul>
-        </>
-    )
-}
+  const changeAuthorHandler = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  return (
+    <>
+      <Modal>
+        <NewPost
+          onBodyChange={changeBodyHandler}
+          onAuthorChange={changeAuthorHandler}
+        />
+      </Modal>
+      <ul className={styles.posts}>
+        <Post author={author} body={text} />
+        <Post author="David" body={"Check it, it is awesome!"} />
+      </ul>
+    </>
+  );
+};
 
 export default PostsList;
