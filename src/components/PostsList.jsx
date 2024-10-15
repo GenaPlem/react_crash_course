bd92661cacdffc309347d57758e23bbe25adeb71
GenaPlem <shainkill1337@gmail.com>
2024-10-15 17:51:04 +0100

    Add handler to hide the Modal

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,6 +7,11 @@ import styles from "./PostsList.module.css";
 const PostsList = () => {
   const [text, setText] = useState("");
   const [author, setAuthor] = useState("");
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+
+  const hideModalHandler = () => {
+    setModalIsVisible(false);
+  };
 
   const changeBodyHandler = (event) => {
     setText(event.target.value);
@@ -18,12 +23,14 @@ const PostsList = () => {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={changeAuthorHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={changeBodyHandler}
+            onAuthorChange={changeAuthorHandler}
+          />
+        </Modal>
+      )}
       <ul className={styles.posts}>
         <Post author={author} body={text} />
         <Post author="David" body={"Check it, it is awesome!"} />
