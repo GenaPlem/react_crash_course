ee75c177696e1ee660cb2c917f5222db72a5d219
GenaPlem <shainkill1337@gmail.com>
2024-10-15 16:19:59 +0100

    Add one more Post to App.jsx and add props to them

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import Post from './components/Post';
 function App() {
 
   return (
-    <Post />
+    <main>
+      <Post author='Maxim' body='React is awesome!'/>
+      <Post author='David' body='Check it, it is awesome!'/>
+    </main>
   )
 }
 
