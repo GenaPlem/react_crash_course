deca0b2541674f068873fc6e80bee377cc2044ee
GenaPlem <shainkill1337@gmail.com>
2024-10-15 17:23:44 +0100

    Add states and handlers to parent component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from './PostsList.module.css'
 
 const PostsList = () => {
+    const [text, setText] = useState('');
+    const [author, setAuthor] = useState('');
+
+    const changeBodyHandler = (event) => {
+        setText(event.target.value);
+    }
+    
+    const changeAuthorHandler = (e) => {
+        setAuthor(e.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
             <ul className={styles.posts}>
-                <Post author='Maxim' body='React is awesome!'/>
-                <Post author='David' body='Check it, it is awesome!'/>
+                <Post author={author} body={text}/>
+                <Post author='David' body={'Check it, it is awesome!'}/>
             </ul>
         </>
     )
