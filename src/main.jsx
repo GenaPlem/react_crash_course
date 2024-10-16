6b57686ae821ba57e89c44ad8e19d008ba510522
GenaPlem <shainkill1337@gmail.com>
2024-10-16 13:04:18 +0100

    Add Router Provider with Browser Routs

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+import NewPost from "./components/NewPost";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/create-post", element: <NewPost /> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
