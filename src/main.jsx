475c26b3f24ca205f1eb15ac8640584d4e2cc631
GenaPlem <shainkill1337@gmail.com>
2024-10-16 15:30:33 +0100

    Add new path for PostDetails

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import Posts, { loader as postsLoader } from "./routes/Posts.jsx";
 import "./index.css";
 import NewPost, { action as newPostAction } from "./routes/NewPost.jsx";
 import RootLayout from "./routes/RootLayout.jsx";
+import PostDetails, {
+  loader as postDetailsLoader,
+} from "./components/PostDetails";
 
 const router = createBrowserRouter([
   {
@@ -17,7 +20,8 @@ const router = createBrowserRouter([
         element: <Posts />,
         loader: postsLoader,
         children: [
-          { path: "/create-post", element: <NewPost />, action: newPostAction },
+          { path: "create-post", element: <NewPost />, action: newPostAction },
+          { path: ":id", element: <PostDetails />, loader: postDetailsLoader },
         ],
       },
     ],
