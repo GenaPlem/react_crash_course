d4e7db9a192541468fda789faa78f625c21b368d
GenaPlem <shainkill1337@gmail.com>
2024-10-16 15:12:36 +0100

    Add useLoaderData instead of states and useEffect

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,52 +1,23 @@
-import { useEffect, useState } from "react";
 import Post from "./Post";
 import styles from "./PostsList.module.css";
+import { useLoaderData } from "react-router-dom";
 
 const PostsList = () => {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      setIsFetching(true);
-      const response = await fetch("http://localhost:8080/posts");
-      const resData = await response.json();
-      setPosts(resData.posts);
-      setIsFetching(false);
-    };
-
-    fetchPosts();
-  }, []);
-
-  const addPostHandler = (postData) => {
-    fetch("http://localhost:8080/posts", {
-      method: "POST",
-      body: JSON.stringify(postData),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setPosts((prevPosts) => [postData, ...prevPosts]);
-  };
+  const posts = useLoaderData();
 
   return (
     <>
-      {!isFetching && posts.length > 0 && (
+      {posts.length > 0 && (
         <ul className={styles.posts}>
           {posts.map((post, index) => (
             <Post author={post.author} body={post.body} key={index} />
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
-        </div>
-      )}
-      {isFetching && (
-        <div style={{ textAlign: "center", color: "white" }}>
-          <p>Loading posts...</p>
         </div>
       )}
     </>
